a44e2cc84d7678604e439f31dd90a45cd5b1ae47
Matt <matthew.t.kissinger@gmail.com>
2022-06-18 15:46:16 -0700

    challenge complete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react"
 
 export default function Card(props) {
-    console.log(props)
     return (
         <div className="card">
-            <img className="card--image" src={require("../images/mtFuji.png")} />
+            <img className="card--image" src={require(`../images/${props.item.imageUrl}`)} alt={props.item.imageUrl}/>
             <div className="card--info">
                 <div className="card--location">
                     <img src={require("../images/locationIcon.png")} alt="location icon"/>
-                    <p className="card--location-name">JAPAN</p>
+                    <p className="card--location-name">{(props.item.location).toUpperCase()}</p>
                     <p className="card--location-google">View on Google Maps</p>
                 </div>
-                <h3 className="card--info-title">Mount Fuji</h3>
-                <p className="card--info-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="card--info-desc">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h3 className="card--info-title">{props.item.title}</h3>
+                <p className="card--info-dates">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="card--info-desc">{props.item.description}</p>
             </div>
         </div>
     )
